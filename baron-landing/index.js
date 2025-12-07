774c113575749667f30a5b901fdca2750cf32111
neo-dev <sei133dirsoa@gmail.com>
2025-12-07 20:35:12 -0300

    Create MVP for Baron Gaming Landing Page.

--- baron-landing/index.js
+++ baron-landing/index.js
@@ -1,1 +1,18 @@
-console.log("Oi mundo!");
+"use strict";
+
+const express = require('express');
+const path = require('path');
+
+function main() {
+        const app = express();
+        const port = 3000;
+
+        app.use(express.static(path.join(__dirname, "public")));
+
+        app.listen(port, () => {
+                console.log(`Servidor estático rodando em http://localhost:${port} !`);
+        });
+}
+
+
+main();
